a55cbb5bfa9bec20080a966b3e6fe0ba9acff0ef
Kevin <vnctkevin@gmail.com>
2022-11-25 19:58:25 +0700

    Troubleshooting on Image Upload and Display

--- jvk/settings.py
+++ jvk/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
